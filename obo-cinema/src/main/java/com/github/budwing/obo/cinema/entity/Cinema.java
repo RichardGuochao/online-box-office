75e922aae6f91d06f4186be804aa0693b9a96683
weatry <weatry@gmail.com>
2022-09-12 21:55:23 +0800

    obo-cinema combines ShardingSphere JDBC and SEATA

--- obo-cinema/src/main/java/com/github/budwing/obo/cinema/entity/Cinema.java
+++ obo-cinema/src/main/java/com/github/budwing/obo/cinema/entity/Cinema.java
@@ -5,26 +5,16 @@ import com.github.budwing.obo.cinema.vo.GeoLocation;
 import lombok.Data;
 import lombok.ToString;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
-@Entity
-@Table(name = "obo_cinema")
 @Data
 @ToString
 public class Cinema implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(name="name")
     private String name;
-    @Embedded
     private Address address;
-    @Embedded
     private GeoLocation location;
     private String telephone;
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cinema_id")
     private List<Hall> halls;
 }
--- obo-cinema/src/main/java/com/github/budwing/obo/cinema/entity/Seat.java
+++ obo-cinema/src/main/java/com/github/budwing/obo/cinema/entity/Seat.java
@@ -3,20 +3,12 @@ package com.github.budwing.obo.cinema.entity;
 import lombok.Data;
 import lombok.ToString;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "obo_seat")
 @Data
 @ToString
 public class Seat {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @ManyToOne
-    private Cinema cinema;
-    @ManyToOne
-    private Hall hall;
+    private Long id;
+    private Integer cinemaId;
+    private Integer hall;
     private Integer hallFloor;
     private Integer seatRow;
     private Integer seatCol;
